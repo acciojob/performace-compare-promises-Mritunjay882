b5e3e0cb07a88aa1765bfef3334e45ec35119522
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-05-01 12:41:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ const apiUrls = [
 ];
 
 // You can write your code here
+const fetchNames = async () => {
+      try {
+        const res = await Promise.all([
+          fetch("https://jsonplaceholder.typicode.com/todos/1"),
+           fetch("https://jsonplaceholder.typicode.com/todos/2"),
+          fetch("https://jsonplaceholder.typicode.com/todos/3"),
+          fetch("https://jsonplaceholder.typicode.com/todos/4"),
+          fetch("https://jsonplaceholder.typicode.com/todos/5"),
+          fetch("https://jsonplaceholder.typicode.com/todos/6"),
+          fetch("https://jsonplaceholder.typicode.com/todos/7"),
+          fetch("https://jsonplaceholder.typicode.com/todos/8"),
+          fetch("https://jsonplaceholder.typicode.com/todos/9"),
+          fetch("https://jsonplaceholder.typicode.com/todos/10")
+        ]);
+        const data = await Promise.all(res.map(r => r.json()))
+        console.log(data.flat());
+      } catch {
+        throw Error("Promise failed");
+      }
+};
